9d2b5bf90d1fbfa1029330b50a2f40fca78aeac2
Michael Luciuk <mrl280@mail.usask.ca>
2021-05-07 11:11:58 -0600

    Updated plotting file structure so that each type of plot has it's own "out" directory

---
+++ DataAnalysis/Plotting/vel_vs_time/RKN_RISR_vel_vs_time.py
@@ -0,0 +1,153 @@
+import calendar
+import time
+import os
+import pathlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+import pandas as pd
+
+if __name__ == '__main__':
+    """
+    Plot SuperDARN and RISR velocity vs time
+    """
+
+    # TODO: Update so it will work with multiple SuperDARN data chunks
+
+    SAVE_PLOT = True
+
+    year = "2014"
+    month = "03"
+    day = "04"
+
+    SD_station = "rkn"
+    SD_beam_range = [5, 5]
+    SD_gate_range = [30, 40]
+
+    RISR_station = "ran"
+    # RISR experiments can run for several days, the whole experiment is in one file labeled with the experiment start
+    # date.  This experiment start date is not necessarily the date we are plotting.
+    RISR_exp_start_month = "3"
+    RISR_exp_start_day = "2"
+    RISR_wd_beam_range = [2, 2]
+
+    # Compute start and end epochs
+    pattern = '%Y.%m.%d %H:%M:%S'  # This is the human readable time pattern we use
+    start_date_time = year + "." + month + "." + day + " " + "00:00:00"
+    end_date_time = year + "." + month + "." + day + " " + "23:59:59"
+    start_epoch = calendar.timegm(time.strptime(start_date_time, pattern))
+    end_epoch = calendar.timegm(time.strptime(end_date_time, pattern))
+
+    # Read in SuperDARN data
+    loc_root = str(((pathlib.Path().parent.absolute()).parent.absolute()).parent.absolute())
+    print(loc_root)
+    SD_in_dir = loc_root + "/DataReading/SD/data/" + SD_station + "/" + SD_station + year + month + day
+    SD_in_file = SD_in_dir + "/" + SD_station + year + month + day + ".pkl"
+    SD_df = pd.read_pickle(SD_in_file)
+
+    # Read in RISR data
+    RISR_in_dir = loc_root + "/DataReading/RISR/data/" + RISR_station + year + RISR_exp_start_month + RISR_exp_start_day
+    RISR_in_file = RISR_in_dir + "/" + RISR_station + year + RISR_exp_start_month + RISR_exp_start_day \
+                   + ".LongPulse.pkl"
+    RISR_df = pd.read_pickle(RISR_in_file)
+
+    # Filter SuperDARN data
+    SD_df = SD_df.loc[(SD_df['time'] >= start_epoch) & (SD_df['time'] <= end_epoch)  # filer times
+                      & (SD_df['gate'] >= SD_gate_range[0]) & (SD_df['gate'] <= SD_gate_range[1])  # filter gates
+                      & (SD_df['bmnum'] >= SD_beam_range[0]) & (SD_df['bmnum'] <= SD_beam_range[1])]  # filter beams
+    SD_df.reset_index(drop=True, inplace=True)
+
+    RISR_df = RISR_df.loc[(RISR_df['time'] >= start_epoch) & (RISR_df['time'] <= end_epoch)  # filer times
+                          & (RISR_df['wd_bmnum'] >= RISR_wd_beam_range[0])
+                          & (RISR_df['wd_bmnum'] <= RISR_wd_beam_range[1])]  # filter beams
+    RISR_df.reset_index(drop=True, inplace=True)
+
+    # Recover decimal times from epoch times
+    SD_decimal_time = []
+    RISR_decimal_time = []
+    for t in range(len(SD_df['time'])):
+        hour = str.split(time.strftime(pattern, time.gmtime(SD_df['time'][t])))[1][0:2]
+        min = str.split(time.strftime(pattern, time.gmtime(SD_df['time'][t])))[1][3:5]
+        sec = str.split(time.strftime(pattern, time.gmtime(SD_df['time'][t])))[1][6:8]
+        SD_decimal_time.append(float(hour) + float(min) / 60.0 + float(sec) / 3600.0)
+    for t in range(len(RISR_df['time'])):
+        hour = str.split(time.strftime(pattern, time.gmtime(RISR_df['time'][t])))[1][0:2]
+        min = str.split(time.strftime(pattern, time.gmtime(RISR_df['time'][t])))[1][3:5]
+        sec = str.split(time.strftime(pattern, time.gmtime(RISR_df['time'][t])))[1][6:8]
+        RISR_decimal_time.append(float(hour) + float(min) / 60.0 + float(sec) / 3600.0)
+    SD_df['decimal_time'] = SD_decimal_time
+    RISR_df['decimal_time'] = RISR_decimal_time
+
+    # Build title strings
+    if SD_beam_range[0] == SD_beam_range[1]:
+        SD_beam_string = "Beam " + str(SD_beam_range[0])
+    else:
+        SD_beam_string = "Beams " + str(SD_beam_range[0]) + "-" + str(SD_beam_range[1])
+    if SD_gate_range[0] == SD_gate_range[1]:
+        SD_gate_string = "Gate " + str(SD_gate_range[0])
+    else:
+        SD_gate_string = "Gates " + str(SD_gate_range[0]) + "-" + str(SD_gate_range[1])
+
+    if RISR_wd_beam_range[0] == RISR_wd_beam_range[1]:
+        RISR_beam_string = "WD Beam " + str(RISR_wd_beam_range[0])
+    else:
+        RISR_beam_string = "WD Beams " + str(RISR_wd_beam_range[0]) + "-" + str(RISR_wd_beam_range[1])
+
+    # Ensure out directory
+    out_dir = loc_root + "/Plotting/vel_vs_time/out/" + year + month + day
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # Loop thorough and plot 2 hour chunks of data
+    for chunk_num in range(2):
+
+        # Computer start and end epochs and build restricted data frames
+        start_hour_here = 0 + 2 * chunk_num
+        end_hour_here = 2 + 2 * chunk_num
+        start_date_time_here = year + "." + month + "." + day + " " + str(0 + 2 * chunk_num) + ":00:00"
+        end_date_time_here = year + "." + month + "." + day + " " + str(1 + 2 * chunk_num) + ":00:00"
+        start_epoch_here = calendar.timegm(time.strptime(start_date_time, pattern))
+        end_epoch_here = calendar.timegm(time.strptime(end_date_time, pattern))
+
+        restricted_SD_df = SD_df[(SD_df['time'] >= start_epoch_here) & (SD_df['time'] <= end_epoch_here)]
+        restricted_RISR_df = RISR_df[(RISR_df['time'] >= start_epoch_here) & (RISR_df['time'] <= end_epoch_here)]
+
+        # Set up the plot
+        fig, ax = plt.subplots(figsize=(8, 6), dpi=300, nrows=2, ncols=1)
+        plt.subplots_adjust(hspace=0.4)
+        fig.suptitle("RKN and RISR LOS Velocity Evolution; as produced by " + str(os.path.basename(__file__)),
+                     fontsize=12.5)
+
+        # Plot SuperDARN data on the first set of axis
+        ax[0].title.set_text(SD_station + "; " + SD_beam_string + "; " + SD_gate_string)
+        ax[0].set_xlabel('Time [UT]')
+        ax[0].set_ylabel('LOS Velocity [m/s]')
+        ax[0].set_ylim([-600, 600])
+        ax[0].set_xlim([start_hour_here, end_hour_here])
+        ax[0].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        ax[0].grid(which='major', axis='y', linestyle='--', linewidth=0.5)
+        ax[0].plot(ax[0].get_ylim(), [0, 0], linestyle='-', linewidth=0.5, color='black')
+        ax[0].plot(restricted_SD_df['decimal_time'], restricted_SD_df['vel'], 'r.', markersize=5)
+        ax[0].errorbar(restricted_SD_df['decimal_time'], restricted_SD_df['vel'], yerr=restricted_SD_df['vel_err'],
+                       fmt='none', color='black', linewidth=0.75)
+
+        # Plot RISR data on the second plot
+        ax[1].title.set_text(RISR_station + "; " + RISR_beam_string)
+        ax[1].set_xlabel('Time [UT]')
+        ax[1].set_ylabel('LOS Ion Velocity [m/s]')
+        ax[1].set_ylim([-600, 600])
+        ax[1].set_xlim([start_hour_here, end_hour_here])
+        ax[1].xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        ax[1].grid(which='major', axis='y', linestyle='--', linewidth=0.5)
+        ax[1].plot(ax[1].get_ylim(), [0, 0], linestyle='-', linewidth=0.5, color='black')
+        ax[1].plot(restricted_RISR_df['decimal_time'], restricted_RISR_df['los_ion_vel'], 'r.', markersize=5)
+        # ax[1].errorbar(restricted_RISR_df['decimal_time'], restricted_RISR_df['los_ion_vel'],
+        #                yerr=restricted_RISR_df['los_ion_vel_err'], fmt='none', color='black', linewidth=0.75)
+
+        # plt.show()
+
+        if SAVE_PLOT:
+            cur_path = os.path.dirname(__file__)  # where we are
+            fig.savefig(out_dir + "/" + SD_station + "_" + RISR_station + "_vel_vs_time_"
+                        + year + month + day + " " + str(start_hour_here) + "-" + str(end_hour_here) + "UT"
+                        + ".pdf", format='pdf', dpi=300)
