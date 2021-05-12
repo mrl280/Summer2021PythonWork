e917c1ff40cd630024222254ae497190b7d15340
Michael Luciuk <mrl280@mail.usask.ca>
2021-05-12 09:40:35 -0600

    Program matches up velocities, right now 30 seconds of observations every 2 gates are combined

--- DataAnalysis/MultiFreqExperiment/VelocityAnalysis/vel_matching.py
+++ DataAnalysis/MultiFreqExperiment/VelocityAnalysis/vel_matching.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 
     station = "rkn"
     start_date_time = year + "-" + month + "-" + day + " " + "00:00:00"
-    end_date_time = year + "-" + month + "-" + day + " " + "04:00:00"
+    end_date_time = year + "-" + month + "-" + day + " " + "08:00:00"
 
     # Compute start and end epoch
     pattern = '%Y-%m-%d %H:%M:%S'
@@ -39,93 +39,125 @@ if __name__ == '__main__':
 
     # Read in SuperDARN data
     loc_root = str(((pathlib.Path().parent.absolute()).parent.absolute()).parent.absolute())
-    SD_in_dir = loc_root + "/DataReading/SD/data/" + station + "/" + station + year + month + day
-    SD_in_file = SD_in_dir + "/" + station + year + month + day + ".pkl"
-    df = pd.read_pickle(SD_in_file)
+    in_dir = loc_root + "/DataReading/SD/data/" + station + "/" + station + year + month + day
+    in_file = in_dir + "/" + station + year + month + day + ".pkl"
+    df = pd.read_pickle(in_file)
 
-    # We are only interested in 15 km resolution data
+    # We are only interested in 15 km resolution data (from the multi-freq analysis)
     # This double filter should be redundant but better to be safe
     df = df.loc[(df['firstRang'] == 90) & (df['rangeSep'] == 15)]
-    df = basic_SD_df_filter(df)  # This re-indexes
-
-    # All data for this experiment is in one beam
-    beam = df['bmnum'][0]
+    df = basic_SD_df_filter(df)
 
     # Filter for a gate range and time
     df = df.loc[(df['gate'] >= gates[0]) & (df['gate'] <= gates[1])]
     df = df.loc[(df['epoch'] >= start_epoch) & (df['epoch'] <= end_epoch)]
+
+    # There should not be any nan velocities, but drop to be safe
+    df = df.loc[df['vel'].notna()]
     df.reset_index(drop=True, inplace=True)
+
+    # All data for this experiment is in one beam
+    beam = df['bmnum'][0]
 
     # Recover decimal time
     print("Recovering decimal time...")
-    decimal_time = []
-    for t in range(len(df['epoch'])):
-        decimal_time.append(df['hour'][t] + df['minute'][t] / 60.0 + df['second'][t] / 3600.0)
-    df['decimalTime'] = decimal_time
+    df['decimalTime'] = df['hour'] + df['minute'] / 60.0 + df['second'] / 3600.0
     print("Number of points in the data frame: " + str(len(df['epoch'])))
 
-    # Initialize arrays
-    start_decimal_times = []
-    vel10 = []
-    vel12 = []
-    vel13 = []
-    vel14 = []
+    # Initialize arrays to hold matched data
+    matched_times = []
+    vel10, count10 = [], []
+    vel12, count12 = [], []
+    vel13, count13 = [], []
+    vel14, count14 = [], []
 
     # Look through the gates in sets of 5
-    for start_gate in range(gates[0], gates[1], 5):
-        end_gate = start_gate + 4
+    for start_gate in range(gates[0], gates[1], 2):
+        end_gate = start_gate + 1
         print("Looking at gates " + str(start_gate) + " to " + str(end_gate))
 
         # Restrict data to with that gate
-        restricted_df = df[(df['gate'] >= start_gate) & (df['gate'] <= end_gate)]
-        print("Number of points here: " + str(len(restricted_df['epoch'])))
+        gate_restricted_df = df[(df['gate'] >= start_gate) & (df['gate'] <= end_gate)]
+        print("Number of points here: " + str(len(gate_restricted_df['epoch'])))
 
         # Loop through decimal time in jumps of 30 seconds
-        time_interval_s = 120  # seconds
-        for start_time in np.arange(0, 4, time_interval_s / 3600):
-            # TODO: Match up points.  There are lots of points in each gate but not each time.
-            #  Might have to remove .loc
-            end_time = start_time + time_interval_s / 3600
-            restricted_df = restricted_df.loc[(restricted_df['decimalTime'] >= start_time)
-                                              & (restricted_df['decimalTime'] < end_time)]
-            start_decimal_times.append(start_time)
+        time_interval_s = 30  # seconds
+        for start_time in np.arange(0, 8, time_interval_s / 3600):
 
-            df_10 = restricted_df.loc[(restricted_df['transFreq'] >= (10 - 0.4) * 1000)
-                                      & (restricted_df['transFreq'] <= (10 + 0.4) * 1000)]
+            end_time = start_time + time_interval_s / 3600
+            time_restricted_df = gate_restricted_df[(gate_restricted_df['decimalTime'] >= start_time)
+                                                    & (gate_restricted_df['decimalTime'] < end_time)]
+            # print("     Number of points here: " + str(len(time_restricted_df['epoch'])))
+
+            # Use the mid-time as the time marker
+            matched_times.append(start_time + 0.5 * time_interval_s / 3600)
+
+            df_10 = time_restricted_df[(time_restricted_df['transFreq'] >= (10 - 0.4) * 1000)
+                                       & (time_restricted_df['transFreq'] <= (10 + 0.4) * 1000)]
             try:
                 vel10.append(statistics.median(df_10['vel']))
+                count10.append(df_10.shape[0])
             except:
                 vel10.append(math.nan)
+                count10.append(math.nan)
 
-            df_12 = restricted_df.loc[(restricted_df['transFreq'] >= (12 - 0.4) * 1000)
-                                      & (restricted_df['transFreq'] <= (12 + 0.4) * 1000)]
+            df_12 = time_restricted_df[(time_restricted_df['transFreq'] >= (12 - 0.4) * 1000)
+                                       & (time_restricted_df['transFreq'] <= (12 + 0.4) * 1000)]
             try:
                 vel12.append(statistics.median(df_12['vel']))
+                count12.append(df_12.shape[0])
             except:
                 vel12.append(math.nan)
+                count12.append(math.nan)
 
-            df_13 = restricted_df.loc[(restricted_df['transFreq'] >= (13 - 0.4) * 1000)
-                                      & (restricted_df['transFreq'] <= (13 + 0.4) * 1000)]
+            df_13 = time_restricted_df[(time_restricted_df['transFreq'] >= (13 - 0.4) * 1000)
+                                       & (time_restricted_df['transFreq'] <= (13 + 0.4) * 1000)]
             try:
                 vel13.append(statistics.median(df_13['vel']))
+                count13.append(df_13.shape[0])
             except:
                 vel13.append(math.nan)
+                count13.append(math.nan)
 
-            df_14 = restricted_df.loc[(restricted_df['transFreq'] >= (14 - 0.4) * 1000)
-                                      & (restricted_df['transFreq'] <= (14 + 0.4) * 1000)]
+            df_14 = time_restricted_df[(time_restricted_df['transFreq'] >= (14 - 0.4) * 1000)
+                                       & (time_restricted_df['transFreq'] <= (14 + 0.4) * 1000)]
             try:
                 vel14.append(statistics.median(df_14['vel']))
+                count14.append(df_14.shape[0])
             except:
                 vel14.append(math.nan)
+                count14.append(math.nan)
 
     # Put the data into a dataframe
-    matched_data = pd.DataFrame({'startTimes': start_decimal_times,
+    matched_data = pd.DataFrame({'decimalTimes': matched_times,
                                  'vel10': vel10,
+                                 'count10': count10,
                                  'vel12': vel12,
+                                 'count12': count12,
                                  'vel13': vel13,
-                                 'vel14': vel14})
+                                 'count13': count13,
+                                 'vel14': vel14,
+                                 'count14': count14
+                                 })
 
-    print(matched_data[matched_data['vel12'].notnull()])
+    matched_data['10over12'] = matched_data['vel10'] / matched_data['vel12']
+    matched_data['13over12'] = matched_data['vel13'] / matched_data['vel12']
+    matched_data['14over12'] = matched_data['vel14'] / matched_data['vel12']
+    # print(matched_data[matched_data['10over12'].notna()][['vel10', 'count10', 'vel12', 'count12', '10over12']])
+
+    # Save the data for later
+    out_dir = loc_root + "/MultiFreqExperiment/VelocityAnalysis/data/" + station
+    out_file = out_dir + "/" + station + year + month + day + ".MatchedData.pkl"
+    print("Pickling as " + out_file + "...")
+    df.to_pickle(out_file)
+
+
+    # matched_data = matched_data.loc[(matched_data['count10'] >= 3) & (matched_data['count12'] >= 3)
+    #                                 & matched_data['10over12'].notna()]
+    # print(matched_data[['vel10', 'count10', 'vel12', 'count12', '10over12']])
+
+
+    # df.loc[df['vel'].notna()]
 
     # print(df['gate'].unique())
     #
