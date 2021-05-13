454c9283503624d6fd78fb6b1cc43fb4672387a2
Michael Luciuk <mrl280@mail.usask.ca>
2021-05-13 11:17:08 -0600

    Adjusted multi-freq matching so that we consider each gate individually

--- DataAnalysis/MultiFreqExperiment/VelocityAnalysis/vel_matching.py
+++ DataAnalysis/MultiFreqExperiment/VelocityAnalysis/vel_matching.py
@@ -31,13 +31,16 @@ if __name__ == '__main__':
 
     year = "2016"  # yyyy
     month = "09"  # mm
-    day = "25"  # dd
-    gates = [10, 74]
-    elv_max = 25  # deg
+    day = "26"  # dd
 
-    station = "inv"
-    start_date_time = year + "-" + month + "-" + day + " " + "00:00:00"
-    end_date_time = year + "-" + month + "-" + day + " " + "08:00:00"
+    station = "rkn"
+    start_hour = 0  # Start and end times must be integer values for the loop
+    end_hour = 4
+    start_date_time = year + "-" + month + "-" + day + " " + str(start_hour) + ":00:00"
+    end_date_time = year + "-" + month + "-" + day + " " + str(end_hour) + ":00:00"
+
+    # elv_max = 25  # deg
+    gates = [10, 74]  # We will match up data over the whole gate range now and just restrict when plotting
 
     # Compute start and end epoch
     pattern = '%Y-%m-%d %H:%M:%S'
@@ -63,46 +66,44 @@ if __name__ == '__main__':
     df = df.loc[df['vel'].notna()]
 
     # Lets make sure we are looking at E region echoes by filtering out high elevation angle data
-    df = df.loc[df['elv'] <= elv_max]
+    # df = df.loc[df['elv'] <= elv_max]
     df.reset_index(drop=True, inplace=True)
-
-    # All data for this experiment is in one beam
-    beam = df['bmnum'][0]
 
     # Recover decimal time
     print("Recovering decimal time...")
     df['decimalTime'] = df['hour'] + df['minute'] / 60.0 + df['second'] / 3600.0
+
     print("Number of points in the data frame: " + str(len(df['epoch'])))
 
     # Initialize arrays to hold matched data
     matched_times = []
-    starting_gates = []
+    matched_gates = []
     vel10, count10 = [], []
     vel12, count12 = [], []
     vel13, count13 = [], []
     vel14, count14 = [], []
 
-    # Look through the gates in sets of 5
-    for start_gate in range(gates[0], gates[1], 2):
-        end_gate = start_gate + 1
-        print("Looking at gates " + str(start_gate) + " to " + str(end_gate))
+    # Look through the gates (we need to maintain 15 km resolution)
+    for start_gate in range(gates[0], gates[1] + 1, 1):
+        end_gate = start_gate
+        print("Looking at gate " + str(start_gate))
 
-        # Restrict data to with that gate
+        # Restrict data to within that gate
         gate_restricted_df = df[(df['gate'] >= start_gate) & (df['gate'] <= end_gate)]
-        print("Number of points here: " + str(len(gate_restricted_df['epoch'])))
+        print("Number of points at this gate: " + str(len(gate_restricted_df['epoch'])))
 
-        # Loop through decimal time in jumps of 30 seconds
+        # Loop through decimal time in jumps
         time_interval_s = 30  # seconds
-        for start_time in np.arange(0, 8, time_interval_s / 3600):
+        time_step_h = time_interval_s / 3600
+        for start_time in np.arange(start_hour, end_hour, time_step_h):
 
-            end_time = start_time + time_interval_s / 3600
+            end_time = start_time + time_step_h
             time_restricted_df = gate_restricted_df[(gate_restricted_df['decimalTime'] >= start_time)
                                                     & (gate_restricted_df['decimalTime'] < end_time)]
-            # print("     Number of points here: " + str(len(time_restricted_df['epoch'])))
 
             # Use the mid-time as the time marker
-            matched_times.append(start_time + 0.5 * time_interval_s / 3600)
-            starting_gates.append(start_gate)
+            matched_times.append(start_time + 0.5 * time_step_h)
+            matched_gates.append(start_gate)
 
             df_10 = time_restricted_df[(time_restricted_df['transFreq'] >= (10 - 0.4) * 1000)
                                        & (time_restricted_df['transFreq'] <= (10 + 0.4) * 1000)]
@@ -142,7 +143,7 @@ if __name__ == '__main__':
 
     # Put the data into a dataframe
     matched_data = pd.DataFrame({'decimalTimes': matched_times,
-                                 'startingGates': starting_gates,
+                                 'gates': matched_gates,
                                  'vel10': vel10,
                                  'count10': count10,
                                  'vel12': vel12,
@@ -156,11 +157,15 @@ if __name__ == '__main__':
     matched_data['10over12'] = matched_data['vel10'] / matched_data['vel12']
     matched_data['13over12'] = matched_data['vel13'] / matched_data['vel12']
     matched_data['14over12'] = matched_data['vel14'] / matched_data['vel12']
+
+    matched_data['12over10'] = matched_data['vel12'] / matched_data['vel10']
+    matched_data['13over10'] = matched_data['vel13'] / matched_data['vel10']
+    matched_data['14over10'] = matched_data['vel14'] / matched_data['vel10']
     # print(matched_data[matched_data['10over12'].notna()][['vel10', 'count10', 'vel12', 'count12', '10over12']])
 
     # Save the data for later
     out_dir = loc_root + "/MultiFreqExperiment/VelocityAnalysis/data/" + station
-    out_file = out_dir + "/" + station + year + month + day + ".MatchedData.pkl"
+    out_file = out_dir + "/" + station + year + month + day + ".MatchedData.1gg30s.pkl"
     print("Pickling as " + out_file + "...")
     matched_data.to_pickle(out_file)
 
