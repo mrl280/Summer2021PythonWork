7fcbdf190b456a213f9b505c1eaf73d24b80fa5e
Michael Luciuk <mrl280@mail.usask.ca>
2021-06-01 10:24:54 -0600

    Filtering for desired times based on epoch, and added in buffer 0 for single digit months and dates

--- DataAnalysis/OneAndOneHalfHop/get_df_multi_event.py
+++ DataAnalysis/OneAndOneHalfHop/get_df_multi_event.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pathlib
-
+import calendar
+import time
 
 def get_df_multi_event(file_name=None, flag=None):
     """
@@ -38,6 +39,8 @@ def get_df_multi_event(file_name=None, flag=None):
             print("Error: " + str(flag) + " is not a recognized key.  "
                   "Your options are: " + str([i for i in event_summary.keys()]))
 
+    pattern = '%Y-%m-%d %H:%M:%S'
+
     number_of_events = len(event_summary)
     if number_of_events <= 0:
         print("There are no events to consider.")
@@ -46,15 +49,30 @@ def get_df_multi_event(file_name=None, flag=None):
         # There is at least one event, use the first event to initialize the dataframe
         station = event_summary['SD_station'][0]
         year = str(event_summary['year'][0])
-        month = str(event_summary['month'][0])
-        day = str(event_summary['day'][0])
+
+        if event_summary['month'][0] >= 10:
+            month = str(event_summary['month'][0])
+        else:
+            month = "0" + str(event_summary['month'][0])
+
+        if event_summary['day'][0] >= 10:
+            day = str(event_summary['day'][0])
+        else:
+            day = "0" + str(event_summary['day'][0])
+
         start_hour_UT = event_summary['start_hour_UT'][0]
         end_hour_UT = event_summary['end_hour_UT'][0]
+
+        # Compute start and end epoch, they are needed to filter the df
+        start_date_time = year + "-" + month + "-" + day + " " + str(start_hour_UT) + ":00:00"
+        end_date_time = year + "-" + month + "-" + day + " " + str(end_hour_UT) + ":00:00"
+        start_epoch = calendar.timegm(time.strptime(start_date_time, pattern))
+        end_epoch = calendar.timegm(time.strptime(end_date_time, pattern))
 
         in_dir = str(loc_root.parent) + "/DataReading/SD/data/" + station + "/" + station + year + month + day
         in_file = in_dir + "/" + station + year + month + day + ".pkl"
         df = pd.read_pickle(in_file)
-        df = df.loc[(df['hour'] >= start_hour_UT) & (df['hour'] <= end_hour_UT)]
+        df = df.loc[(df['epoch'] >= start_epoch) & (df['epoch'] <= end_epoch)]
         df.reset_index(drop=True, inplace=True)
 
         # Loop through every other event and add the data to the dataframe
@@ -62,15 +80,30 @@ def get_df_multi_event(file_name=None, flag=None):
 
             station = event_summary['SD_station'][event]
             year = str(event_summary['year'][event])
-            month = str(event_summary['month'][event])
-            day = str(event_summary['day'][event])
+
+            if event_summary['month'][0] >= 10:
+                month = str(event_summary['month'][0])
+            else:
+                month = "0" + str(event_summary['month'][0])
+
+            if event_summary['day'][0] >= 10:
+                day = str(event_summary['day'][0])
+            else:
+                day = "0" + str(event_summary['day'][0])
+
             start_hour_UT = event_summary['start_hour_UT'][event]
             end_hour_UT = event_summary['end_hour_UT'][event]
+
+            # Compute start and end epoch, they are needed to filter the df
+            start_date_time = year + "-" + month + "-" + day + " " + str(start_hour_UT) + ":00:00"
+            end_date_time = year + "-" + month + "-" + day + " " + str(end_hour_UT) + ":00:00"
+            start_epoch = calendar.timegm(time.strptime(start_date_time, pattern))
+            end_epoch = calendar.timegm(time.strptime(end_date_time, pattern))
 
             in_dir = str(loc_root.parent) + "/DataReading/SD/data/" + station + "/" + station + year + month + day
             in_file = in_dir + "/" + station + year + month + day + ".pkl"
             next_df = pd.read_pickle(in_file)
-            next_df = next_df.loc[(next_df['hour'] >= start_hour_UT) & (next_df['hour'] <= end_hour_UT)]
+            next_df = next_df.loc[(next_df['epoch'] >= start_epoch) & (next_df['epoch'] <= end_epoch)]
             next_df.reset_index(drop=True, inplace=True)
 
             df = pd.concat([df, next_df])  # Warning: this is costly if there are a large number of events
