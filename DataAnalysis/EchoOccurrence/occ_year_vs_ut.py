1630dce0520eeed72a2f5c7de38e7d6be8047bb5
Michael Luciuk <mrl280@mail.usask.ca>
2021-06-08 09:32:38 -0600

    Fixed up a few problems

--- DataAnalysis/EchoOccurrence/occ_year_vs_ut.py
+++ DataAnalysis/EchoOccurrence/occ_year_vs_ut.py
@@ -1,5 +1,8 @@
 import pathlib
 import warnings
+
+import numpy as np
+import pandas as pd
 import pydarn
 
 from matplotlib import pyplot as plt
@@ -21,6 +24,7 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
     Notes:
         - This program was originally written to be run on maxwell.usask.ca.  This decision was made because
             occurrence investigations often require chewing large amounts of data.
+        - Only considers 45 km data
         - Does not distinguish frequency
         - This program uses fitACF 3.0 data.  To change this, modify the source code.
         - All times and dates are assumed UT
@@ -58,7 +62,7 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
     year_range = check_year_range(year_range)
     hour_range = check_hour_range(hour_range)
     month_range = (1, 12)
-    day_range = (1, 31)
+    day_range = (1, 2)  # TODO: Fix back to (1, 31)
 
     if isinstance(station, str):
         hdw_info = pydarn.read_hdw_file(station)  # Get the hardware file, there is lots of good stuff in there
@@ -73,7 +77,10 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
         # Just read in some test data
         warnings.warn("Running in local testing mode, just going to use local dummy data", category=Warning)
         # df = get_local_dummy_data(station=station, year=2011, month=9, day=29, start_hour_UT=0, end_hour_UT=23)
-        df = get_local_dummy_data(station=station, year=2014, month=3, day=3, start_hour_UT=0, end_hour_UT=23)
+        df = get_local_dummy_data(station=station, year=2011, month=11, day=12, start_hour_UT=0, end_hour_UT=24)
+        df_2 = get_local_dummy_data(station=station, year=2011, month=9, day=29, start_hour_UT=0, end_hour_UT=24)
+        df_3 = get_local_dummy_data(station=station, year=2012, month=10, day=15, start_hour_UT=0, end_hour_UT=24)
+        df = pd.concat([df, df_2, df_3])
         # print(df.keys())
     else:
         df = get_data(station=station, year_range=year_range, month_range=month_range, day_range=day_range,
@@ -82,8 +89,16 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
     df = df.loc[(df['p_l'] >= 3)]  # Restrict to points with at least 3 dB
     if parameter is not None:
         df = df.loc[(df[parameter] >= zmin) & (df[parameter] <= zmax)]
-
     df.reset_index(drop=True, inplace=True)
+
+    # Restrict to 45 km mode data, you need to modify the fan if you want to use data of a different spatial resolution
+    number_of_records_before_spatial_resolution_check = df.shape[0]
+    df = df.loc[(df['frang'] == 180) & (df['rsep'] == 45)]
+    df.reset_index(drop=True, inplace=True)
+    number_of_records_after_spatial_resolution_check = df.shape[0]
+    if number_of_records_before_spatial_resolution_check > number_of_records_after_spatial_resolution_check:
+        warnings.warn("Not all data within the specified range is 45 km resolution data.  "
+                      "All non-45 km data was discarded.", category=Warning)
 
     # We will have one subplot for every year of observations
     n_rows = (year_range[1] + 1) - year_range[0]
@@ -94,7 +109,7 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
 
     # Apply common subplot formatting
     ax[n_rows - 1].set_xlabel('Time, UT')
-    for row in range(n_rows):
+    for row in reversed(range(n_rows)):
         ax[row].set_ylim([0, 13])
         ax[row].set_xlim(hour_range)
         ax[row].tick_params(axis='y', which='major', labelleft=False, direction='in')
@@ -103,14 +118,15 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
         ax[row].yaxis.set_major_locator(MultipleLocator(6.5))  # Half way through the year
         ax[row].grid(b=True, which='major', axis='x', linestyle='--', linewidth=0.5, zorder=4)
         ax[row].grid(b=True, which='major', axis='y', linestyle='--', linewidth=0.5, zorder=4)
-        ax[row].set_ylabel(year_range[0] + row)
+        ax[row].set_ylabel(year_range[1] - row)
 
     # Loop through the years, adding in the data
     n_bins_y = 24  # half month bins
     n_bins_x = (hour_range[1] - hour_range[0]) * 2  # half hour bins
     contour_range = [ax[0].get_xlim(), ax[0].get_ylim()]  # All plots are the same size
-    for row in range(n_rows):
-        year = year_range[0] + row
+    for row in reversed(range(n_rows)):
+        print(row)
+        year = year_range[1] - row
         start_datetime, start_epoch = build_date_epoch(year, 1, 1, 0)
         end_datetime, end_epoch = build_date_epoch(year, 12, 31, 24)
 
@@ -121,13 +137,19 @@ def occ_year_vs_ut(station, year_range, hour_range=None, gate_range=None, beam_r
 
         # Compute decimal time to plot along x, and decimal datetime to plot along y
         df_yy['xdata'] = df_yy['hour'] + df_yy['minute'] / 60 + df_yy['second'] / 3600
-        df_yy['ydata'] = (df_yy['month'] - 1) / 12 + (df_yy['day'] - 1) / 365 + df_yy['xdata'] / 8760
+        df_yy['ydata'] = (df_yy['month'] - 1) + (df_yy['day'] - 1) / 31 + df_yy['xdata'] / 730
 
-        # TODO: Compute the median of a parameter if one is given
-
-        binned_counts, bin_xedges, bin_yedges, bin_numbers = stats.binned_statistic_2d(
-            df_yy['xdata'], df_yy['ydata'], values=None,
-            statistic='count', bins=[n_bins_x, n_bins_y], range=contour_range)
+        if parameter is None:
+            # We just want a simple echo count
+            binned_counts, bin_xedges, bin_yedges, bin_numbers = stats.binned_statistic_2d(
+                df_yy['xdata'], df_yy['ydata'], values=None,
+                statistic='count', bins=[n_bins_x, n_bins_y], range=contour_range)
+        else:
+            # We want to the median of the chosen parameter
+            binned_counts, bin_xedges, bin_yedges, bin_numbers = stats.binned_statistic_2d(
+                df_yy['xdata'], df_yy['ydata'], values=df_yy[parameter],
+                statistic='median', bins=[n_bins_x, n_bins_y], range=contour_range)
+            binned_counts = np.nan_to_num(binned_counts)
 
         # Compute bin centers
         bin_xwidth = (bin_xedges[1] - bin_xedges[0])
@@ -146,14 +168,14 @@ if __name__ == '__main__':
     """ Testing """
 
     station = "rkn"
-    fig = occ_year_vs_ut(station=station, year_range=(2013, 2015),
+    fig = occ_year_vs_ut(station=station, year_range=(2011, 2012),
                          gate_range=(20, 30), beam_range=(7, 7),
-                         parameter=None, local_testing=False)
+                         parameter='v', local_testing=True)
 
     loc_root = str((pathlib.Path().parent.absolute()))
     out_dir = loc_root + "/out"
     out_file = out_dir + "/occ_year_vs_ut_" + station
     print("Saving plot as " + out_file)
-    fig.savefig(out_file + ".jpg", format='jpg', dpi=300)
+    # fig.savefig(out_file + ".jpg", format='jpg', dpi=300)
 
     plt.show()
