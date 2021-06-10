4e530621f8116bed48aea66a9d60a5c6343d3978
Michael Luciuk <mrl280@mail.usask.ca>
2021-06-10 11:42:16 -0600

    General program cleanup and refactor

--- DataAnalysis/EchoOccurrence/occ_year_vs_time.py
+++ DataAnalysis/EchoOccurrence/occ_year_vs_time.py
@@ -9,18 +9,20 @@ from pydarn import radar_fov, SuperDARNRadars
 from scipy import stats
 
 from DataAnalysis.EchoOccurrence.lib.add_mlt_to_df import add_mlt_to_df
+from DataAnalysis.EchoOccurrence.lib.cm.modified_viridis import modified_viridis_2
 from lib.only_keep_45km_res_data import only_keep_45km_res_data
 from lib.get_data_handler import get_data_handler
 from lib.z_min_max_defaults import z_min_max_defaults
 from lib.build_datetime_epoch import build_datetime_epoch
-from lib.data_getters.range_checkers import *
+from lib.data_getters.input_checkers import *
 
 
-def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_range=None, beam_range=None,
-                   local_testing=False, parameter=None):
+def occ_year_vs_ut(station, year_range, month_range=None, time_units='mlt', hour_range=None, gate_range=None, beam_range=None,
+                   parameter=None, local_testing=False):
     """
 
     Produce a contour plot with year on the y-axis and time along the x-axis.
+    Plots a simple echo count
 
     Notes:
         - This program was originally written to be run on maxwell.usask.ca.  This decision was made because
@@ -35,6 +37,8 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
             For a complete listing of available stations, please see https://superdarn.ca/radar-info
     :param year_range: (<int>, <int>):
             Inclusive. The year range to consider.
+    :param month_range: (<int>, <int>) (optional):
+            Inclusive. The months of the year to consider.  If omitted (or None), then all days will be considered.
     :param time_units: str: 'ut' for universal time or 'mlt' for magnetic local time:
             The time units to plot along x.  Default is 'mlt'
     :param hour_range: (<int>, <int>) (optional):
@@ -46,29 +50,21 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
     :param beam_range: (<int>, <int>) (optional):
             Inclusive. The beam range to consider.  If omitted (or None), then all beams will be considered.
             Note that beams start at 0, so beams (0, 3) is 4 beams.
-    :param local_testing: bool (optional):
-            Set this to true if you are testing on your local machine.  Program will then use local dummy data.
     :param parameter: str (optional):
             Parameter to be averaged (e.g. 'v' or 'p_l')
             If omitted, then a simple echo count will be plotted.
+    :param local_testing: bool (optional):
+            Set this to true if you are testing on your local machine.  Program will then use local dummy data.
     :return: matplotlib.pyplot.figure
             The figure can then be modified, added to, printed out, or saved in whichever file format is desired.
     """
 
-    if time_units.upper() == "MLT":
-        time_units = "mlt"
-    elif time_units.upper() == "UT":
-        time_units = "ut"
-    else:
-        raise Exception("invalid time units passed to occ_year_vs_ut().  Options are 'mlt' or 'ut'.")
-
-    if parameter is not None:
-        zmin, zmax = z_min_max_defaults(parameter)
+    time_units = check_time_units(time_units)
+    hour_range = check_hour_range(hour_range)
 
     print("Retrieving data...")
     # TODO: Decide what to do about month and day ranges
-    hour_range = check_hour_range(hour_range)
-    df = get_data_handler(station, year_range=year_range, month_range=(1, 12), day_range=(1, 31),
+    df = get_data_handler(station, year_range=year_range, month_range=month_range, day_range=(1, 31),
                           hour_range=hour_range, gate_range=gate_range, beam_range=beam_range,
                           local_testing=local_testing)
 
@@ -79,10 +75,20 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
     print("Filtering data...")
     df = df.loc[(df['p_l'] >= 3)]  # Restrict to points with at least 3 dB
     if parameter is not None:
+        zmin, zmax = z_min_max_defaults(parameter)
         df = df.loc[(df[parameter] >= zmin) & (df[parameter] <= zmax)]
 
-    df.reset_index(drop=True, inplace=True)
+        if parameter == 'v':
+            cmap = 'seismic_r'
+            levels = np.linspace(zmin, zmax, 13, endpoint=True)
+        else:
+            cmap = modified_viridis_2()
+            levels = 6
+    else:
+        cmap = modified_viridis_2()
+        levels = 6
 
+    df.reset_index(drop=True, inplace=True)
     df = only_keep_45km_res_data(df)
 
     print("Preparing the plot...")
@@ -90,25 +96,22 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
     fig, ax = plt.subplots(figsize=(8, 9), sharex='col', dpi=300, nrows=n_rows, ncols=1)
     plt.subplots_adjust(hspace=0.05)
 
-    if time_units == "mlt":
-        ax[n_rows - 1].set_xlabel('Time, MLT')
-    elif time_units == "ut":
-        ax[n_rows - 1].set_xlabel('Time, UT')
-
     # Apply common subplot formatting
+    ax[n_rows - 1].set_xlabel("Time, " + time_units.upper(), fontsize=18)
     for row in reversed(range(n_rows)):
         ax[row].set_ylim([0, 13])
         ax[row].set_xlim(hour_range)
         ax[row].tick_params(axis='y', which='major', labelleft=False, direction='in')
         ax[row].tick_params(axis='x', which='major', direction='in')
+        plt.xticks(fontsize=16)
         ax[row].xaxis.set_major_locator(MultipleLocator(2))  # Every 2 hours
         ax[row].yaxis.set_major_locator(MultipleLocator(6.5))  # Half way through the year
         ax[row].grid(b=True, which='major', axis='x', linestyle='--', linewidth=0.5, zorder=4)
         ax[row].grid(b=True, which='major', axis='y', linestyle='--', linewidth=0.5, zorder=4)
-        ax[row].set_ylabel(year_range[1] - row)
+        ax[row].set_ylabel(year_range[1] - row, fontsize=18)
 
     # Loop through the years, adding in the data
-    n_bins_y = 24  # half month bins
+    n_bins_y = 48  # half month bins
     n_bins_x = (hour_range[1] - hour_range[0]) * 2  # half hour bins
     contour_range = [ax[0].get_xlim(), ax[0].get_ylim()]  # All plots are the same size
     for row in reversed(range(n_rows)):
@@ -120,6 +123,8 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
         df_yy = df.loc[(df['epoch'] >= start_epoch) & (df['epoch'] <= end_epoch)].copy()
         df_yy.reset_index(drop=True, inplace=True)
 
+        df_yy['ut_time'] = df_yy['hour'] + df_yy['minute'] / 60 + df_yy['second'] / 3600
+
         if time_units == "mlt":
             print("Computing MLTs for " + str(year) + " data...")
 
@@ -129,20 +134,20 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
             beam_corners_aacgm_lats, beam_corners_aacgm_lons = \
                 radar_fov(stid=radar_id, coords='aacgm', date=mid_datetime)
 
-            df = add_mlt_to_df(beam_corners_aacgm_lons=beam_corners_aacgm_lons,
-                               beam_corners_aacgm_lats=beam_corners_aacgm_lats, df=df)
+            df_yy = add_mlt_to_df(beam_corners_aacgm_lons=beam_corners_aacgm_lons,
+                                  beam_corners_aacgm_lats=beam_corners_aacgm_lats, df=df_yy)
 
-            df_yy = df_yy.loc[(df_yy['mlt'] >= hour_range[0]) & (df_yy['mlt'] <= hour_range[1])]
             df_yy['xdata'] = df_yy['mlt']
         else:
-            # Use UT
-            df_yy = df_yy.loc[(df_yy['hour'] >= hour_range[0]) & (df_yy['hour'] <= hour_range[1])]
-            df_yy['xdata'] = df_yy['hour'] + df_yy['minute'] / 60 + df_yy['second'] / 3600
+            df_yy['xdata'] = df_yy['ut_time']
 
+        df_yy = df_yy.loc[(df_yy['xdata'] >= hour_range[0]) & (df_yy['xdata'] <= hour_range[1])]
         df_yy.reset_index(drop=True, inplace=True)
 
         # Compute decimal datetime to plot along y
-        df_yy['ydata'] = (df_yy['month'] - 1) + (df_yy['day'] - 1) / 31 + df_yy['xdata'] / 730
+        df_yy['ydata'] = (df_yy['month'] - 1) + (df_yy['day'] - 1) / 31 + df_yy['ut_time'] / 730
+
+        print(df_yy['ut_time'])
 
         if parameter is None:
             # We just want a simple echo count
@@ -163,9 +168,12 @@ def occ_year_vs_ut(station, year_range, time_units='mlt', hour_range=None, gate_
         bin_ycenters = bin_yedges[1:] - bin_ywidth / 2
 
         # Plot the data
-        cont = ax[row].contourf(bin_xcenters, bin_ycenters, binned_counts.transpose(), 5)
-        fig.colorbar(cont, ax=ax[row])
+        cont = ax[row].contourf(bin_xcenters, bin_ycenters, binned_counts.transpose(),
+                                cmap=cmap, levels=levels)
+        cbar = fig.colorbar(cont, ax=ax[row], shrink=0.75)
+        cbar.ax.tick_params(labelsize=16)
 
+    print("Returning the figure...")
     return fig
 
 
@@ -175,9 +183,9 @@ if __name__ == '__main__':
     local_testing = True
 
     station = "rkn"
-    fig = occ_year_vs_ut(station=station, time_units='mlt', year_range=(2011, 2012),
-                         gate_range=(20, 30), beam_range=(7, 7),
-                         parameter='v', local_testing=local_testing)
+    fig = occ_year_vs_ut(station=station, time_units='mlt', year_range=(2011, 2012), month_range=(1, 12),
+                         gate_range=(30, 74), beam_range=(7, 7),
+                         parameter=None, local_testing=local_testing)
 
     if local_testing:
         plt.show()
