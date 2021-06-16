957568e12cc38a61d6aef8fdf3b811865ccf0587
Michael Luciuk <mrl280@mail.usask.ca>
2021-06-16 15:32:34 -0600

    Updated to plot for both ionospheric scatter and ground scatter on 2 subplots

--- DataAnalysis/EchoOccurrence/occ_full_circle.py
+++ DataAnalysis/EchoOccurrence/occ_full_circle.py
@@ -1,3 +1,4 @@
+import calendar
 import math
 import pathlib
 import pydarn
@@ -22,7 +23,7 @@ from lib.data_getters.input_checkers import *
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 
-def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=None, beam_range=None,
+def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=None, beam_range=None, freq_range=None,
                     time_units='mlt', plot_type='contour', local_testing=False):
     """
 
@@ -52,6 +53,9 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
     :param beam_range: (<int>, <int>) (optional):
             Inclusive. The beam range to consider.  If omitted (or None), then all beams will be considered.
             Note that beams start at 0, so beams (0, 3) is 4 beams.
+    :param freq_range: (<float>, <float>) (optional):
+            Inclusive.  The frequency range to consider in MHz.
+            If omitted (or None), then all frequencies are considered.
     :param time_units: str: 'ut' for universal time or 'mlt' for magnetic local time:
             The time units to plot on the circle, 12 is always at the top.  Default is 'mlt'
     :param plot_type: str (optional):
@@ -64,6 +68,8 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
 
     time_units = check_time_units(time_units)
     year = check_year(year)
+    month_range = check_month_range(month_range)
+    freq_range = check_freq_range(freq_range)
 
     all_radars_info = SuperDARNRadars()
     this_radars_info = all_radars_info.radars[pydarn.read_hdw_file(station).stid]  # Grab radar info
@@ -77,12 +83,16 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
 
     print("Retrieving data...")
     df = get_data_handler(station, year_range=(year, year), month_range=month_range, day_range=day_range,
-                          gate_range=gate_range, beam_range=beam_range, occ_data=True,
+                          gate_range=gate_range, beam_range=beam_range, freq_range=freq_range, occ_data=True,
                           local_testing=local_testing)
     df = only_keep_45km_res_data(df)
 
-    # To compute mlt we need longitudes.. use the middle of the month as magnetic field estimate
-    date_time_est, _ = build_datetime_epoch(year, 6, 15, 0)
+    if month_range[0] == month_range[1]:
+        month_string = calendar.month_name[month_range[0]]
+        date_time_est, _ = build_datetime_epoch(year, month_range[0], 15, 0)
+    else:
+        month_string = calendar.month_name[month_range[0]] + " to " + calendar.month_name[month_range[1]]
+        date_time_est, _ = build_datetime_epoch(year, 6, 15, 0)  # Use the middle of the year as an estimate
 
     print("Computing MLTs for " + str(year) + " data...")
     cell_corners_aacgm_lats, cell_corners_aacgm_lons = radar_fov(stid=radar_id, coords='aacgm', date=date_time_est)
@@ -111,53 +121,69 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
 
         df['xdata'] = np.asarray(ut_time)
 
-    print("Preparing the plot...")
-    fig = plt.figure(figsize=(5, 5), dpi=300)
-    lat_extreme = 40 * hemisphere.value  # deg
-    if hemisphere.value == 1:
-        ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
-    elif hemisphere.value == -1:
-        ax = fig.add_subplot(1, 1, 1, projection=ccrs.SouthPolarStereo())
-    else:
-        raise Exception("hemisphere not recognized")
-
-    ax.set_extent([-180, 180, hemisphere.value * 90, lat_extreme], crs=ccrs.PlateCarree())
-
     # Compute a circle in axis coordinates which can be used as a boundary
     theta = np.linspace(0, 2 * np.pi, 100)
     center, radius = [0.5, 0.5], 0.5
     vertices = np.vstack([np.sin(theta), np.cos(theta)]).T
     circle = mpath.Path(vertices * radius + center)
-    ax.set_boundary(circle, transform=ax.transAxes)
 
-    # Add gridlines and mlt labels
-    text_offset_multiplier = 1.03
-    gl = ax.gridlines(draw_labels=True, linestyle='--', zorder=5)
-    gl.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135])
-    gl.xformatter = LONGITUDE_FORMATTER
-    gl.yformatter = LATITUDE_FORMATTER
-
-    # Print clock numbers
-    ax.text(0, text_offset_multiplier * ax.get_ylim()[1], "12", ha='center', va='bottom')
-    ax.text(0, text_offset_multiplier * ax.get_ylim()[0], "00", ha='center', va='top')
-    ax.text(text_offset_multiplier * ax.get_xlim()[1], 0, "06", ha='left', va='center')
-    ax.text(text_offset_multiplier * ax.get_xlim()[0], 0, "18", ha='right', va='center')
-
-    # Print time units
-    ax.text(text_offset_multiplier * ax.get_xlim()[0],
-            text_offset_multiplier * ax.get_ylim()[1], time_units.upper(), ha='left', va='bottom')
-
-    # Convert radar coordinates to aacgm and plot radar track
+    # Convert radar coordinates to aacgm
     radar_lat_aacgm, radar_lon_aacgm, radar_mlt = get_aacgm_coord(radar_lat, radar_lon, 0, date_time_est)
     radar_mlts = np.arange(0, 360, 1)
     radar_lats_aacgm = np.asarray([radar_lat_aacgm] * len(radar_mlts))
-    ax.plot(radar_mlts, radar_lats_aacgm, color='k', linewidth=0.5, linestyle="--", transform=ccrs.Geodetic(),
-            label="Radar Path")
+
+    lat_extreme = 40 * hemisphere.value  # deg
+    text_offset_multiplier = 1.03
+    if hemisphere.value == 1:
+        projection = ccrs.NorthPolarStereo()
+    elif hemisphere.value == -1:
+        projection = ccrs.SouthPolarStereo()
+    else:
+        raise Exception("hemisphere not recognized")
+
+    print("Preparing the plot...")
+    fig, ax = plt.subplots(figsize=[10, 6], dpi=300, nrows=1, ncols=2, subplot_kw={'projection': projection})
+    plt.subplots_adjust(left=0, right=1, bottom=0, top=0.85, wspace=0)
+    fig.suptitle(month_string + " " + str(year) + " at " + station.upper() +
+                 "; Beams " + str(beam_range[0]) + "-" + str(beam_range[1]) +
+                 "; Frequencies " + str(freq_range[0]) + "-" + str(freq_range[1]) + " MHz", fontsize=18)
+
+    # Apply common subplot formatting
+    for i in range(ax.size):
+
+        ax[i].set_extent([-180, 180, hemisphere.value * 90, lat_extreme], crs=ccrs.PlateCarree())
+        ax[i].set_boundary(circle, transform=ax[i].transAxes)
+
+        # Add gridlines
+        gl = ax[i].gridlines(draw_labels=True, linestyle='--', zorder=5)  # Note: Labels wont draw on a circular axis
+        gl.xlocator = mticker.FixedLocator([-180, -135, -90, -45, 0, 45, 90, 135])
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+        # Print clock numbers
+        ax[i].text(0, text_offset_multiplier * ax[i].get_ylim()[1], "12", ha='center', va='bottom')
+        ax[i].text(0, text_offset_multiplier * ax[i].get_ylim()[0], "00", ha='center', va='top')
+        ax[i].text(text_offset_multiplier * ax[i].get_xlim()[1], 0, "06", ha='left', va='center')
+        ax[i].text(text_offset_multiplier * ax[i].get_xlim()[0], 0, "18", ha='right', va='center')
+
+        # Print out the time units
+        ax[i].text(ax[i].get_xlim()[1], ax[i].get_ylim()[1],
+                   time_units.upper(), ha='left', va='bottom')
+
+        # Plot radar track
+        ax[i].plot(radar_mlts, radar_lats_aacgm, color='k', linewidth=0.5, linestyle="--", transform=ccrs.Geodetic())
+
+    # Print out echo types
+    ax[0].text(ax[0].get_xlim()[0], ax[0].get_ylim()[1],
+               "IS", ha='left', va='bottom')
+    ax[1].text(ax[1].get_xlim()[0], ax[1].get_ylim()[1],
+               "GS", ha='left', va='bottom')
+
+    print("Computing binned occ rates...")
 
     # Right now xdata is in the range 0-24, we need to put it in the range 0-360 for circular plotting
     df['xdata'] = 15 * df['xdata']
 
-    print("Computing binned occ rates...")
     # Compute mlt edges
     deg_mlt_per_bin = 2
     n_bins_mlt = int(360 / deg_mlt_per_bin)
@@ -169,8 +195,11 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
     lat_edges = np.linspace(lat_extreme, 90, num=(n_bins_lat + 1))
     delta_lat = lat_edges[1] - lat_edges[0]
 
-    contour_data = np.empty(shape=(n_bins_mlt, n_bins_lat))
-    contour_data[:] = math.nan
+    contour_data_is = np.empty(shape=(n_bins_mlt, n_bins_lat))
+    contour_data_is[:] = math.nan
+
+    contour_data_gs = np.empty(shape=(n_bins_mlt, n_bins_lat))
+    contour_data_gs[:] = math.nan
 
     for mlt_idx, start_mlt in enumerate(mlt_edges):
         if start_mlt == mlt_edges[-1]:
@@ -186,16 +215,16 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
             df_mlt_lat = df_mlt[(df_mlt['lat'] >= start_lat) & (df_mlt['lat'] <= end_lat)]
 
             try:
-                contour_data[mlt_idx][lat_idx] = sum(df_mlt_lat['good_echo']) / len(df_mlt_lat)
+                contour_data_is[mlt_idx][lat_idx] = sum(df_mlt_lat['good_iono_echo']) / len(df_mlt_lat)
+                contour_data_gs[mlt_idx][lat_idx] = sum(df_mlt_lat['good_grndscat_echo']) / len(df_mlt_lat)
             except ZeroDivisionError:
                 # There are no point in this interval
-                contour_data[mlt_idx, lat_idx] = math.nan
+                contour_data_is[mlt_idx][lat_idx] = math.nan
+                contour_data_gs[mlt_idx][lat_idx] = math.nan
             except BaseException as e:
                 print("MLT index: " + str(mlt_idx))
                 print("LAT index: " + str(lat_idx))
                 raise e
-
-    contour_range = [[0, 360], [hemisphere.value * 37, hemisphere.value * 90]]
 
     # Compute bin centers
     bin_xwidth = (mlt_edges[1] - mlt_edges[0])
@@ -206,16 +235,25 @@ def occ_full_circle(station, year, month_range=None, day_range=None, gate_range=
     levels = 12
     levels = np.linspace(start=0, stop=1, num=(levels + 1))
     if plot_type == "contour":
-        plot = ax.contourf(bin_xcenters, bin_ycenters, contour_data.transpose(), cmap='jet', levels=levels, transform=ccrs.PlateCarree())
+        plot0 = ax[0].contourf(bin_xcenters, bin_ycenters, contour_data_is.transpose(),
+                            cmap='jet', levels=levels, transform=ccrs.PlateCarree())
+        plot1 = ax[1].contourf(bin_xcenters, bin_ycenters, contour_data_gs.transpose(),
+                            cmap='jet', levels=levels, transform=ccrs.PlateCarree())
 
     elif plot_type == "pixel":
-        plot = ax.imshow(np.flip(contour_data.transpose(), axis=0), aspect='auto', cmap="jet", transform=ccrs.PlateCarree())
+        plot0 = ax[0].imshow(np.flip(contour_data_is.transpose(), axis=0), aspect='auto',
+                         cmap="jet", transform=ccrs.PlateCarree(), vmin=0, vmax=1)
+        plot1 = ax[1].imshow(np.flip(contour_data_is.transpose(), axis=0), aspect='auto',
+                             cmap="jet", transform=ccrs.PlateCarree(), vmin=0, vmax=1)
 
     else:
         raise Exception("plot_type not recognized")
 
-    cbar = fig.colorbar(plot, ax=ax, shrink=0.75, orientation="horizontal", format='%.2f')
-    cbar.ax.tick_params(labelsize=14, labelrotation=30)
+    cbar0 = fig.colorbar(plot0, ax=ax[0], shrink=0.7, orientation="horizontal", format='%.2f')
+    cbar0.ax.tick_params(labelsize=14, labelrotation=30)
+
+    cbar1 = fig.colorbar(plot1, ax=ax[1], shrink=0.7, orientation="horizontal", format='%.2f')
+    cbar1.ax.tick_params(labelsize=14, labelrotation=30)
 
     return df, fig
 
@@ -228,9 +266,9 @@ if __name__ == '__main__':
     if local_testing:
         station = "rkn"
 
-        df, fig = occ_full_circle(station=station, year=2011, month_range=None, day_range=None, time_units='mlt',
-                                  gate_range=(0, 74), beam_range=None, plot_type='contour',
-                                  local_testing=local_testing)
+        _, fig = occ_full_circle(station=station, year=2011, month_range=None, day_range=None, time_units='mlt',
+                                 gate_range=(0, 74), beam_range=None, plot_type='contour',
+                                 local_testing=local_testing)
 
         plt.show()
 
@@ -240,9 +278,9 @@ if __name__ == '__main__':
         year = 2011
         month = 2
 
-        df, fig = occ_full_circle(station=station, year=year, month_range=None, day_range=None, time_units='mlt',
-                                  gate_range=(0, 74), beam_range=(13, 15), plot_type='contour',
-                                  local_testing=local_testing)
+        _, fig = occ_full_circle(station=station, year=year, month_range=None, day_range=None, time_units='mlt',
+                                 gate_range=(0, 74), beam_range=(13, 15), plot_type='contour',
+                                 local_testing=local_testing)
 
         loc_root = str((pathlib.Path().parent.absolute()))
         out_dir = loc_root + "/out"
