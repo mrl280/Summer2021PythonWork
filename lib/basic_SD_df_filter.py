3badba4679abf1ab1d78e472f6111a455b51ef75
Michael Luciuk <mrl280@mail.usask.ca>
2021-07-15 09:41:31 -0600

    Updated docstring

--- lib/basic_SD_df_filter.py
+++ lib/basic_SD_df_filter.py
@@ -2,13 +2,18 @@
 
 def basic_SD_df_filter(df):
     """
-    Performs the basic standard filtering one might wish to perform on SuperDARN data
+    Performs the basic standard filtering one usually needs to perform on SuperDARN data.
+    Operation is completed inplace.
+
     Only keeps SuperDARN data where:
         - Ground scatter flag is 0
         - Quality flag is 1
         - Power is >= 3 dB
-    :param df: SuperDARN data frame
-    :return: SuperDARN data frame filtered for ground scatter, bad quality data, and low power data. Data is re-indexed.
+
+    :param df: pandas.DataFrame:
+        SuperDARN data frame
+    :return df: pandas.DataFrame:
+        SuperDARN data frame filtered for ground scatter, bad quality data, and low power data. Data is re-indexed.
     """
     try:
         df = df.loc[(df['gflg'] == 0) & (df['qflg'] == 1) & (df['pwr'] >= 3.0)]
@@ -16,4 +21,4 @@ def basic_SD_df_filter(df):
         return df
     except Exception as e:
         print("Error in basic_SD_df_filter(): ")
-        print(e)
+        raise e
