a54fdb7b25d6e41e8249567040d9e97658471824
Michael Luciuk <mrl280@mail.usask.ca>
2021-07-16 11:21:22 -0600

    Width and power contour plots complete

--- DataAnalysis/MultiFreqExperiment/Sept26EventDetailedInvestigation/watermann.py
+++ DataAnalysis/MultiFreqExperiment/Sept26EventDetailedInvestigation/watermann.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 from matplotlib import pyplot as plt
-from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import MultipleLocator, NullFormatter
 from scipy import stats
 
 from DataAnalysis.DataReading.SD.elevation_v2 import elevation_v2
@@ -17,7 +17,7 @@ from DataAnalysis.EchoOccurrence.lib.build_datetime_epoch import build_datetime_
 from lib.basic_SD_df_filter import basic_SD_df_filter
 
 
-def watermann(single_day_df, area=None):
+def watermann(single_day_df, axes_vlimits, area=None):
     """
 
     Produce three plots:
@@ -27,6 +27,8 @@ def watermann(single_day_df, area=None):
 
     :param single_day_df: pandas.Data_Frame:
             A single days worth of data that we want to range profile
+    :param axes_vlimits: dict:
+            Dictionary of vlim (zlim) ranges keyed by plot type.
     :param area: int:
             The numbered area of interest.
 
@@ -47,8 +49,6 @@ def watermann(single_day_df, area=None):
                    'count': "Normalized Echo Count",
                    'wdt': "Spectral Width [m/s]",
                    'pwr': "Power [dB]"}
-    axes_vlimits = {'wdt': (0, 120),
-                    'pwr': (0, 120)}
     cbar_labelsize = 12
 
     print("Filtering data..")
@@ -101,11 +101,76 @@ def watermann(single_day_df, area=None):
         add_in_count_data(ax=axes[freq]['count'], df_ff=df_ff)
 
         # Add width contours
-        reference_plot = add_in_wdt_data(ax=axes[freq]['wdt'], df_ff=df_ff, vlims=axes_vlimits['wdt'])
-        cbar = fig.colorbar(reference_plot, cax=axes['cbar']['wdt'], shrink=0.75, format='%d', aspect=5)
-        cbar.ax.tick_params(labelsize=cbar_labelsize)
+        wdt_reference_plot = add_in_wdt_data(ax=axes[freq]['wdt'], df_ff=df_ff, vlims=axes_vlimits['wdt'])
+        cbar_wdt = fig.colorbar(wdt_reference_plot, cax=axes['cbar']['wdt'], shrink=0.75, format='%d')
+        cbar_wdt.ax.tick_params(labelsize=cbar_labelsize)
+
+        # Add power contours
+        pwr_reference_plot = add_in_pwr_data(ax=axes[freq]['pwr'], df_ff=df_ff, vlims=axes_vlimits['pwr'])
+        cbar_pwr = fig.colorbar(pwr_reference_plot, cax=axes['cbar']['pwr'], shrink=0.75, format='%d')
+        cbar_pwr.ax.tick_params(labelsize=cbar_labelsize)
+
+        # Add in gridlines
+        for keys, ax in axes[freq].items():
+            ax.grid(b=True, which='major', axis='both', linestyle='--', linewidth=0.7, zorder=4)
 
     return fig
+
+
+def add_in_pwr_data(ax, df_ff, vlims):
+    """
+    Plot width vs doppler
+
+    :param vlims: (int, int):
+            Colour bar limits
+    :param: fig: matplotlib.pyplot.figure:
+            The figure to use
+    :param ax: matplotlib.pyplot.axis:
+            The axis to draw on
+    :param df_ff: pandas.DataFrame:
+            The frequency restricted dataframe to use
+
+    :return plot: matplotlib.pyplot.plot:
+            An example plot that can be referenced when adding the colour bar
+    """
+
+    if len(df_ff) <= 0:
+        return
+
+    vel_range = ax.get_xlim()
+    pwr_range = ax.get_ylim()
+
+    n_levels = 12
+    levels = np.linspace(start=vlims[0], stop=vlims[1], num=(n_levels + 1))
+
+    # Compute velocity edges
+    vel_bin_width = 10  # m/s
+    n_bins_vel = int((vel_range[1] - vel_range[0]) / vel_bin_width)
+    vel_edges = np.linspace(vel_range[0], vel_range[1], num=(n_bins_vel + 1))
+
+    # Compute power edges
+    pwr_bin_width = 2
+    n_bins_y = int((pwr_range[1] - pwr_range[0]) / pwr_bin_width)
+    pwr_edges = np.linspace(pwr_range[0], pwr_range[1], num=(n_bins_y + 1))
+
+    # Compute bin centers
+    delta_vel = vel_edges[1] - vel_edges[0]
+    delta_pwr = pwr_edges[1] - pwr_edges[0]
+    binned_xcenters = vel_edges[1:] - delta_vel / 2
+    binned_ycenters = pwr_edges[1:] - delta_pwr / 2
+
+    result, _, _, _ = stats.binned_statistic_2d(df_ff['vel'], df_ff['pwr'], values=None, statistic='count',
+                                                bins=[vel_edges, pwr_edges])
+
+    # Replace the zeros with nans so no data shows up as white
+    result[result == 0] = np.nan
+
+    plot = ax.contourf(binned_xcenters, binned_ycenters, result.transpose(), cmap='jet', levels=levels, extend='max')
+
+    # Mark our 3 dB power cutoff
+    ax.axhline(y=3, color='black', linestyle='-', linewidth=1, zorder=3)
+
+    return plot
 
 
 def add_in_wdt_data(ax, df_ff, vlims):
@@ -135,12 +200,12 @@ def add_in_wdt_data(ax, df_ff, vlims):
     levels = np.linspace(start=vlims[0], stop=vlims[1], num=(n_levels + 1))
 
     # Compute velocity edges
-    vel_bin_width = 20  # m/s
+    vel_bin_width = 10  # m/s
     n_bins_vel = int((vel_range[1] - vel_range[0]) / vel_bin_width)
     vel_edges = np.linspace(vel_range[0], vel_range[1], num=(n_bins_vel + 1))
 
     # Compute width edges
-    wdt_bin_width = 10
+    wdt_bin_width = 5
     n_bins_y = int((wdt_range[1] - wdt_range[0]) / wdt_bin_width)
     wdt_edges = np.linspace(wdt_range[0], wdt_range[1], num=(n_bins_y + 1))
 
@@ -150,19 +215,13 @@ def add_in_wdt_data(ax, df_ff, vlims):
     binned_xcenters = vel_edges[1:] - delta_vel / 2
     binned_ycenters = wdt_edges[1:] - delta_wdt / 2
 
-    print("Here are the vel edges:")
-    print(vel_edges)
-
-    print("Here are the wdt edges:")
-    print(wdt_edges)
-
     result, _, _, _ = stats.binned_statistic_2d(df_ff['vel'], df_ff['wdt'], values=None, statistic='count',
                                                 bins=[vel_edges, wdt_edges])
 
     # Replace the zeros with nans so no data shows up as white
     result[result == 0] = np.nan
 
-    plot = ax.contourf(binned_xcenters, binned_ycenters, result.transpose(), cmap='jet', levels=levels)
+    plot = ax.contourf(binned_xcenters, binned_ycenters, result.transpose(), cmap='jet', levels=levels, extend='max')
 
     return plot
 
@@ -193,25 +252,25 @@ def add_axes(fig):
         A dictionary of data axes
     """
 
-    gs = fig.add_gridspec(ncols=5, nrows=3)
+    gs = fig.add_gridspec(nrows=3, ncols=25)
 
     axes = dict()
-    axes[10] = {"count": fig.add_subplot(gs[0, 0]),
-                "wdt": fig.add_subplot(gs[1, 0]),
-                "pwr": fig.add_subplot(gs[2, 0])}
-    axes[12] = {"count": fig.add_subplot(gs[0, 1]),
-                "wdt": fig.add_subplot(gs[1, 1]),
-                "pwr": fig.add_subplot(gs[2, 1])}
-    axes[13] = {"count": fig.add_subplot(gs[0, 2]),
-                "wdt": fig.add_subplot(gs[1, 2]),
-                "pwr": fig.add_subplot(gs[2, 2])}
-    axes[14] = {"count": fig.add_subplot(gs[0, 3]),
-                "wdt": fig.add_subplot(gs[1, 3]),
-                "pwr": fig.add_subplot(gs[2, 3])}
+    axes[10] = {"count": fig.add_subplot(gs[0, 0:6]),
+                "wdt": fig.add_subplot(gs[1, 0:6]),
+                "pwr": fig.add_subplot(gs[2, 0:6])}
+    axes[12] = {"count": fig.add_subplot(gs[0, 6:12]),
+                "wdt": fig.add_subplot(gs[1, 6:12]),
+                "pwr": fig.add_subplot(gs[2, 6:12])}
+    axes[13] = {"count": fig.add_subplot(gs[0, 12:18]),
+                "wdt": fig.add_subplot(gs[1, 12:18]),
+                "pwr": fig.add_subplot(gs[2, 12:18])}
+    axes[14] = {"count": fig.add_subplot(gs[0, 18:24]),
+                "wdt": fig.add_subplot(gs[1, 18:24]),
+                "pwr": fig.add_subplot(gs[2, 18:24])}
 
     # Leave some axes for the colour bars
-    axes['cbar'] = {"wdt": fig.add_subplot(gs[1, 4]),
-                    "pwr": fig.add_subplot(gs[2, 4])}
+    axes['cbar'] = {"wdt": fig.add_subplot(gs[1, 24]),
+                    "pwr": fig.add_subplot(gs[2, 24])}
 
     return axes
 
@@ -244,19 +303,21 @@ def format_subplots(axes, axes_limits, axes_labels):
             # ax.yaxis.set_minor_locator(MultipleLocator(1))
 
             ax.set_xlim(x_lim)
-            # ax.xaxis.set_major_locator(MultipleLocator(0.5))
-            # ax.xaxis.set_minor_locator(MultipleLocator(0.1))
+            ax.xaxis.set_major_locator(MultipleLocator(200))
+            ax.xaxis.set_minor_locator(MultipleLocator(100))
 
+            # Turn off labels on all but the left border plots
             if freq == 10:
                 y_label = axes_labels[subplot_type]
                 ax.set_ylabel(y_label, fontsize=label_font_size)
             else:
-                ax.yaxis.set_ticks([])
+                ax.yaxis.set_major_formatter(NullFormatter())
 
+            # Turn off labels on all but the bottom border plots
             if subplot_type == 'pwr':
                 ax.set_xlabel(x_label, fontsize=label_font_size)
             else:
-                ax.xaxis.set_ticks([])
+                ax.xaxis.set_major_formatter(NullFormatter())
 
             if subplot_type == 'count':
                 ax.set_title(str(freq) + " MHz", fontsize=title_font_size)
@@ -268,6 +329,8 @@ if __name__ == "__main__":
     testing = True
 
     area = None
+    axes_vlimits = {'wdt': (0, 25),
+                    'pwr': (0, 50)}
 
     station = "rkn"
     year = "2016"
@@ -296,7 +359,7 @@ if __name__ == "__main__":
     _, end_epoch = build_datetime_epoch(year=int(year), month=int(month), day=int(day), hour=end_hour)
     df = df.loc[(df['epoch'] >= start_epoch) & (df['epoch'] <= end_epoch)]
 
-    fig = watermann(single_day_df=df, area=area)
+    fig = watermann(single_day_df=df, axes_vlimits=axes_vlimits, area=area)
 
     if testing:
         plt.show()
