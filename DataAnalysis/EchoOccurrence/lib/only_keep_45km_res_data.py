4eac2a4c7a961eb93f0e73882ad74eba83aee3a0
Michael Luciuk <mrl280@mail.usask.ca>
2021-07-19 13:55:07 -0600

    Wrapped restriction in a try block encase the required columns are not found

--- DataAnalysis/EchoOccurrence/lib/only_keep_45km_res_data.py
+++ DataAnalysis/EchoOccurrence/lib/only_keep_45km_res_data.py
@@ -6,14 +6,23 @@ def only_keep_45km_res_data(df):
     Restrict to 45 km mode data.
     Warns if non-45 km data is being removed
 
-    :param df: pandas.DataFrame: Dataframe with 'frang' and 'rsep' parameters
-    :return: pandas.DataFrame: The provided dataframe, except with only 45 km resolution data
+    :param df: pandas.DataFrame:
+            SuperDARN dataframe with 'frang' and 'rsep' parameters.  If these columns don't exist nothing will happen
+            and a warning will be printing.
+    :return: pandas.DataFrame:
+            The provided dataframe, except with only 45 km resolution data
     """
 
     number_of_records_before_spatial_resolution_check = df.shape[0]
 
-    df = df.loc[(df['frang'] == 180) & (df['rsep'] == 45)]
-    df.reset_index(drop=True, inplace=True)
+    try:
+        df = df.loc[(df['frang'] == 180) & (df['rsep'] == 45)]
+        df.reset_index(drop=True, inplace=True)
+    except KeyError:
+        warnings.warn("only_keep_45km_res_data() was not able to restrict your data because the columns 'frang' "
+                      "and 'rsep' could not be found.", category=Warning)
+    except BaseException as e:
+        raise e
 
     number_of_records_after_spatial_resolution_check = df.shape[0]
     if number_of_records_before_spatial_resolution_check > number_of_records_after_spatial_resolution_check:
