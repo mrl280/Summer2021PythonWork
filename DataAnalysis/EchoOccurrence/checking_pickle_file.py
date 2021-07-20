0b3c460227146921fb66da7b03af688869cb6af8
Michael Luciuk <mrl280@mail.usask.ca>
2021-07-20 12:57:42 -0600

    Adding checks to see if there is data for each month that we expect there to be

--- DataAnalysis/EchoOccurrence/checking_pickle_file.py
+++ DataAnalysis/EchoOccurrence/checking_pickle_file.py
@@ -28,4 +28,26 @@ if __name__ == '__main__':
     print("The final datetime in the df is:")
     print(ending_datetime)
 
+    year = []
+    month = []
+    for i in range(len(df)):
+        datetime_obj = df['datetime'].iat[i]
+        year.append(datetime_obj.year)
+        month.append(datetime_obj.month)
+
+    df['year'] = year
+    df['month'] = month
+
+    years = [2019, 2020, 2021]
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    for y in years:
+        print("Now looking at year=" + str(y))
+        df_yy = df.loc[(df['year'] == y)]
+
+        for m in months:
+            df_mm = df_yy.loc[(df_yy['month'] == m)]
+            print("     For month " + str(m) + " len(df) =" + str(len(df_mm)))
+
+
+
 
