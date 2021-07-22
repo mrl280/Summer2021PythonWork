4db09a509e00c5fdcde805daf3757b0dfd615538
Michael Luciuk <mrl280@mail.usask.ca>
2021-07-22 15:47:09 -0600

    No change

--- DataAnalysis/MultiFreqExperiment/Sept26EventDetailedInvestigation/height_ratios_full.py
+++ DataAnalysis/MultiFreqExperiment/Sept26EventDetailedInvestigation/height_ratios_full.py
@@ -260,13 +260,13 @@ if __name__ == '__main__':
     # end_hour = 4
 
     station = "rkn"
-    area = 5  # options: [1, 2, 3, 4, 5, "3a", "3b", "3c"]
+    area = "3c"  # options: [1, 2, 3, 4, 5, "3a", "3b", "3c"]
     year = "2016"  # yyyy
     month = "09"  # mm
     day = "26"  # dd
     gate_range = (0, 74)
     beam_range = (7, 7)
-    data_match_type = "Median"  # "Median" or "Raw"
+    data_match_type = "Raw"  # "Median" or "Raw"
     count_min = 4  # Only used for median matched data
     start_hour = 0
     end_hour = 4
