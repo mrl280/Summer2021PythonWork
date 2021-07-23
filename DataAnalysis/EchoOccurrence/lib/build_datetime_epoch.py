c37e2dccba68fc4e248f2e3fe32d571e866d4bcd
Michael Luciuk <mrl280@mail.usask.ca>
2021-07-23 11:37:36 -0600

    Re-wrote the datetime and epoch builder to it supports minutes and seconds

--- DataAnalysis/EchoOccurrence/lib/build_datetime_epoch.py
+++ DataAnalysis/EchoOccurrence/lib/build_datetime_epoch.py
@@ -3,44 +3,45 @@ import time
 import calendar
 
 
-def build_datetime_epoch(year, month, day, hour):
+def build_datetime_epoch(year, month, day, hour, minute=None, second=None):
     """
-    :param year: int: the year to consider
-    :param month: int: the month to consider
-    :param day: int: the day to consider
-    :param hour: int: The hour to consider
+    Build a datetime struct and compute epoch from raw date/time data.
+
+    :param year: int: Y
+    :param month: int: m
+    :param day: d
+    :param hour: H
+    :param minute: M (optional)
+    :param second: S (optional)
+
     :return: time.struct_time, int: The datetime and epoch
     """
-    pattern = '%Y.%m.%d %H:%M:%S'
 
-    if month < 10:
-        month = "0" + str(month)
-    else:
-        month = str(month)
-    if day < 10:
-        day = "0" + str(day)
-    else:
-        day = str(day)
-    if hour < 10:
-        hour = "0" + str(hour)
-    else:
-        hour = str(hour)
+    pattern = "%Y.%m.%d %H:%M:%S"  # This is the pattern we will use to convert time info to epoch
 
-    if hour == "24":
-        date_time_str = str(year) + "." + str(month) + "." + str(day) \
-            + " " + "23:59:59"
-    else:
-        date_time_str = str(year) + "." + str(month) + "." + str(day) \
-            + " " + str(hour) + ":00:00"
-    date_time_struct = time.strptime(date_time_str, pattern)
+    if minute is None:
+        minute = "00"
+    if second is None:
+        second = "00"
+
+    datetime_here_str = str(year) + "." + str(month) + "." + str(day) + " " + \
+                        str(hour) + ":" + str(minute) + ":" + str(second)
+
+    date_time_struct = time.strptime(datetime_here_str, pattern)
     epoch = calendar.timegm(date_time_struct)
-
     date_time = datetime.datetime.fromtimestamp(time.mktime(date_time_struct))
 
     return date_time, epoch
 
 
 if __name__ == "__main__":
-    date_time, epoch = build_datetime_epoch(2018, 1, 1, 1)
+    """ Testing """
+    date_time, epoch = build_datetime_epoch(year=2018, month=1, day=1, hour=1)
+    print("Datetime: " + str(date_time))
+    print("epoch: " + str(epoch))
+
+    date_time, epoch = build_datetime_epoch(year=2018, month=1, day=1, hour=1, minute=6, second=3)
+    print("Datetime: " + str(date_time))
+    print("epoch: " + str(epoch))
 
     print(type(date_time))
