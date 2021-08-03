987dc538d6c1b06acd6e2cb97802b826aa96074d
Michael Luciuk <mrl280@mail.usask.ca>
2021-08-03 10:50:06 -0600

    Adding pandas check

--- DataAnalysis/Testing/check_library_versions.py
+++ DataAnalysis/Testing/check_library_versions.py
@@ -1,7 +1,10 @@
 import matplotlib
+import pandas
 import _pickle
 
 print('matplotlib: {}'.format(matplotlib.__version__))
 
-# Check to see what pickle protocols are available
-print('_pickle highest protocol: {}'.format(_pickle.HIGHEST_PROTOCOL))
+print('pandas: {}'.format(pandas.__version__))
+
+# # Check to see what pickle protocols are available
+# print('_pickle highest protocol: {}'.format(_pickle.HIGHEST_PROTOCOL))
